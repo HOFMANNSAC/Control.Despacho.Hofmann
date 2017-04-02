1613c8240add952fbef389515dcd40f026504971
econtreras <econtreras@INFORMATICADELL>
2017-04-02 20:42:46 -0300

    Primer UP

---
+++ Movil.Control.Despacho/Movil.Control.Despacho/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Movil.Control.Despacho
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Movil.Control.Despacho.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
